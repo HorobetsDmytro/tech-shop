99da00a24c213d022794e721d924cc70a6ded9d2
HorobetsDmytro <dmtrgorobec@gmail.com>
2024-09-28 18:31:32 +0300

    added pagination, sorting and filtering

--- tech-shop/Controllers/HomeController.cs
+++ tech-shop/Controllers/HomeController.cs
@@ -2,29 +2,57 @@ using Microsoft.AspNetCore.Mvc;
 using tech_shop.Interfaces;
 using tech_shop.Models;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using tech_shop.Repositories;
 
 namespace tech_shop.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
 
-        public HomeController(IProductRepository productRepository)
+        public HomeController(IProductRepository productRepository, ICategoryRepository categoryRepository)
         {
             _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
         }
 
-        public IActionResult Index(int page = 1)
+        public IActionResult Index(int page = 1, string sortOrder = "", int? categoryId = null)
         {
             int pageSize = 8;
-            var allProducts = _productRepository.GetProducts();
-            int totalItems = allProducts.Count();
+            var query = _productRepository.GetProducts();
+
+            // Фільтрація за категорією
+            if (categoryId.HasValue)
+            {
+                query = query.Where(p => p.CategoryId == categoryId.Value);
+            }
+
+            // Сортування
+            switch (sortOrder)
+            {
+                case "price_asc":
+                    query = query.OrderBy(p => p.Price);
+                    break;
+                case "price_desc":
+                    query = query.OrderByDescending(p => p.Price);
+                    break;
+                default:
+                    query = query.OrderBy(p => p.Name);
+                    break;
+            }
+
+            int totalItems = query.Count();
             int totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
 
-            var paginatedProducts = allProducts.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            var paginatedProducts = query.Skip((page - 1) * pageSize).Take(pageSize).ToList();
 
             ViewBag.CurrentPage = page;
             ViewBag.TotalPages = totalPages;
+            ViewBag.SortOrder = sortOrder;
+            ViewBag.CategoryId = categoryId;
+            ViewBag.Categories = _categoryRepository.GetAll(); // Припускаємо, що у вас є метод GetAll() у CategoryRepository
 
             return View(paginatedProducts);
         }
